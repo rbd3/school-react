095da8e65d78f1c21baabce2f463314204488c4e
rbd3 <rabedesanaandry01@gmail.com>
2024-05-12 10:46:54 -0600

    Style logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,15 +6,16 @@ function Menu() {
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid"> <a class="navbar-brand" href="index.html">
       <img class="logo" src="media/logo.png" alt="logo"></img>
-      <p class="max-font-26"><span>RBD School</span><br></br> 2023</p>
+      <p class="max-font-26"><span>RBD School</span></p>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    </div>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto"> <li class="nav-item">
-          <a class="nav-link max-font-26" href="about.html">Home</a>
+          <a class="nav-link max-font-26" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link max-font-26" href="#">About</a>
@@ -28,7 +29,6 @@ function Menu() {
         </li>
       </ul>
     </div>
-  </div>
 </nav>
     );
 }
