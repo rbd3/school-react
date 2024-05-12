37940aede7ecb01d480838c84f653f22d3e4f902
rbd3 <rabedesanaandry01@gmail.com>
2024-05-12 11:24:51 -0600

    Add greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
+import Greeting from './components/Greeting';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
      <Menu />
+     <Greeting />
 
      <Footer />
     </>
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Greeting() {
+    return (   
+        <section id="headline" className="section">
+            <div className="greeting wrapper">
+                <p>"Hello, Sharing world!"</p>
+            </div>
+            <div className="wrapper">
+                <h1>RABEDESANA high School<br/>For Technology<br/> 2024</h1>
+                <p className="description">RBD School is a premier high school in Madagascar specializing in computer science,
+                    English as the main language, and chess is part of the program.
+                    This school will help you or your children to be international workers in the future. 
+                    Join us today and discover the benefits of our curriculum.
+                </p>
+
+                <p className="date">2023.05.22(Mon) ~ 26(FRI)</p>
+                <p className="place">@ Port-Berge, Madagascar</p>
+            </div>
+        </section>
+    );
+}
+
+export default Greeting;
