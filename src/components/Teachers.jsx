7335488d30a814ca845cf380bb9a3339a9115066
rbd3 <rabedesanaandry01@gmail.com>
2024-05-13 07:39:14 -0600

    Update teachers

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -4,52 +4,65 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../assets/teachers.css';
 
+const teachers = [
+  {
+    image: 'media/Me.jpg',
+    name: 'RABEDESANA Andry Narson',
+    occupation: 'Founder and Teacher',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque alias',
+  },
+  {
+    image: 'media/Me.jpg',
+    name: 'RABEDESANA Andry Narson',
+    occupation: 'Founder and Teacher',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque alias',
+  },
+
+  {
+    image: 'media/Me.jpg',
+    name: 'RABEDESANA Andry Narson',
+    occupation: 'Founder and Teacher',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque alias',
+  },
+  {
+    image: 'media/Me.jpg',
+    name: 'RABEDESANA Andry Narson',
+    occupation: 'Founder and Teacher',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque alias',
+  },
+  
+];
+
 function Teachers() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    nextArrow: (
-        <div>
-          <div className="next-slick-arrow">
-              <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="24" viewBox="0 -960 960 960" width="24"><path d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/></svg>
-          </div>
-        </div>
-      ),
-  
-      prevArrow: (
-        <div>
-          <div className="next-slick-arrow rotate-180">
-            <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="24" viewBox="0 -960 960 960" width="24"><path d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/></svg>
-          </div>
-        </div>
-      ),
+    autoplaySpeed: 2500,
   };
 
   return (
-    <>
+    <section id="teachers">
       <h2 className="teachers-title">Featured Teachers</h2>
       <Slider {...settings}>
-        <div>
-          <h3 className='caroussel-wrapper'>Slide 1</h3>
-        </div>
-        <div>
-          <h3>Slide 2</h3>
-        </div>
-        <div>
-          <h3>Slide 3</h3>
-        </div>
-        <div>
-          <h3>Slide 4</h3>
-        </div>
+        {teachers.map((teacher, index) => (
+          <div key={index}>
+            <div className="teachers-card flex">
+              <img src={teacher.image} alt="" className="teachers-img" />
+              <div className="teachers-info flex column">
+                <h3 className="teachers-name">{teacher.name}</h3>
+                <h4 className="teachers-occ">{teacher.occupation}</h4>
+                <hr />
+                <p className="teachers-para">{teacher.info}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </Slider>
-    </>
+    </section>
   );
 }
-
 
 export default Teachers;
