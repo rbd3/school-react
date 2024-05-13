e3f673ff1581ec7aa6951ec588acc39dd3b76e42
rbd3 <rabedesanaandry01@gmail.com>
2024-05-13 00:16:08 -0600

    Update greeting

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,7 +8,7 @@ function Greeting() {
                 <p>"Hello, Sharing world!"</p>
             </div>
             <div className="wrapper">
-                <h1>RABEDESANA high School<br/>For Technology<br/> 2024</h1>
+                <h1 className="h1-with-background">RABEDESANA high School<br/>For Technology<br/> 2024</h1>
                 <p className="description">RBD School is a premier high school in Madagascar specializing in computer science,
                     English as the main language, and chess is part of the program.
                     This school will help you or your children to be international workers in the future. 
