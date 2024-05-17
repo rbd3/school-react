aacde5fade68a04e3d6b13bbc757d218d03f5831
rbd3 <rabedesanaandry01@gmail.com>
2024-05-17 06:02:55 -0600

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,21 @@
-
 import React from 'react';
 import '../assets/footer.css';
 
 function Footer() {
   return (
-    <footer className="wrapper">
+    <footer className="footer-wrapper">
       <a href="index.html">
         <img className="logo" src="media/logo.png" alt="logo" />
         <p className="max-font-26 footer-logo">RBD School</p>
       </a>
       <div className="footer-links">
-      <p className="max-font-26">
-        2023 | RABEDESANA High School
-        <span>
-          <br /><br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima recusandae voluptate. Numquam esse cumque consequatur culpa, veniam sunt,
-          vero repellat, consequuntur itaque maiores fugit sequi ullam exercitationem repudiandae magni?
-        </span>
-      </p>
+        <p className="max-font-26">
+        | RABEDESANA High School
+          <span>
+            <br /><br />
+            Copyright © 2024
+          </span>
+        </p>
       </div>
     </footer>
   );
